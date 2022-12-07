23afb0687771828b8050ba4f87dea3a558f4d39a
unknown <robertcrossland@protonmail.com>
2022-12-06 19:07:22 -0600

    new tests

--- src/search/__tests__/string-to-searches.test.ts
+++ src/search/__tests__/string-to-searches.test.ts
@@ -2,374 +2,228 @@ import { getSearches } from "../string-to-searches";
 import { Book } from "../types";
 
 describe("string-to-searches", () => {
-  // Book Searches
-  const bookGenesis = "Genesis";
-  const bookGenesisExpected: Book[] = [{ name: "Genesis", chapters: [] }];
-  const book1John = "1John";
-  const book1JohnExpected: Book[] = [{ name: "1 John", chapters: [] }];
-  const bookSongOfSolomon = "Song of Solomon";
-  const bookSongOfSolomonExpected: Book[] = [
-    { name: "Song of Solomon", chapters: [] },
-  ];
-  const book11John = "11 John"; // this should fail
-  const book11JohnExpected: Book[] = [];
-
-  // Book Chapter Searches
-  const bookChapterGenesis = "Genesis 1";
-  const bookChapterGenesisExpected: Book[] = [
-    {
-      name: "Genesis",
-      chapters: [{ number: 1, verses: [] }],
-    },
-  ];
-
-  const bookChapter1John = "1John11";
-  const bookChapter1JohnExpected: Book[] = [
-    { name: "1 John", chapters: [{ number: 11, verses: [] }] },
-  ];
-
-  const bookChapterSongOfSolomon = "Song of Solomon 111";
-  const bookChapterSongOfSolomonExpected: Book[] = [
-    { name: "Song of Solomon", chapters: [{ number: 111, verses: [] }] },
-  ];
-
-  // Book Chapter:Verse Searches
-  const bookChapterVerseGenesis = "Genesis 1:1";
-  const bookChapterVerseGenesisExpected: Book[] = [
-    {
-      name: "Genesis",
-      chapters: [
-        {
-          number: 1,
-          verses: [{ number: 1 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterVerse1John = "1 John 11:11";
-  const bookChapterVerse1JohnExpected: Book[] = [
-    {
-      name: "1 John",
-      chapters: [
-        {
-          number: 11,
-          verses: [{ number: 11 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterVerseSongOfSolomon = "Song Of Solomon 111:111";
-  const bookChapterVerseSongOfSolomonExpected: Book[] = [
-    {
-      name: "Song Of Solomon",
-      chapters: [
-        {
-          number: 111,
-          verses: [{ number: 111 }],
-        },
-      ],
-    },
-  ];
-
-  // Book ChapterRange Searches
-  const bookChapterRangeGenesis = "Genesis 1-3";
-  const bookChapterRangeGenesisExpected: Book[] = [
-    {
-      name: "Genesis",
-      chapters: [
-        {
-          number: 1,
-          verses: [],
-        },
-        {
-          number: 2,
-          verses: [],
-        },
-        {
-          number: 3,
-          verses: [],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterRange1John = "1 John 11 - 13";
-  const bookChapterRange1JohnExpected: Book[] = [
-    {
-      name: "1 John",
-      chapters: [
-        {
-          number: 11,
-          verses: [],
-        },
-        {
-          number: 12,
-          verses: [],
-        },
-        {
-          number: 13,
-          verses: [],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterRangeSongOfSolomon = "Song of Solomon111 - 113";
-  const bookChapterRangeSongOfSolomonExpected: Book[] = [
-    {
-      name: "Song of Solomon",
-      chapters: [
-        {
-          number: 111,
-          verses: [],
-        },
-        {
-          number: 112,
-          verses: [],
-        },
-        {
-          number: 113,
-          verses: [],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterVerseRangeGenesis = "Genesis 1:1-3";
-  const bookChapterVerseRangeGenesisExpected: Book[] = [
-    {
-      name: "Genesis",
-      chapters: [
-        {
-          number: 1,
-          verses: [{ number: 1 }, { number: 2 }, { number: 3 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterVerseRange1John = "1  John  11 :  11  -  13";
-  const bookChapterVerseRange1JohnExpected: Book[] = [
-    {
-      name: "1 John",
-      chapters: [
-        {
-          number: 11,
-          verses: [{ number: 11 }, { number: 12 }, { number: 13 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterVerseRangeSongOfSolomon = "Song of Solomon111:111-113";
-  const bookChapterVerseRangeSongOfSolomonExpected: Book[] = [
-    {
-      name: "Song of Solomon",
-      chapters: [
-        {
-          number: 111,
-          verses: [{ number: 111 }, { number: 112 }, { number: 113 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterNonConsecutiveGenesis = "Genesis 1, 3, 5";
-  const bookChapterNonConsecutiveGenesisExpected: Book[] = [
-    {
-      name: "Genesis",
-      chapters: [
-        {
-          number: 1,
-        },
-        {
-          number: 3,
-        },
-        {
-          number: 5,
-        },
-      ],
-    },
-  ];
-
-  const bookChapterNonConsecutive1John = "1  John  9,  11,  13";
-  const bookChapterNonConsecutive1JohnExpectation: Book[] = [
-    {
-      name: "1 John",
-      chapters: [
-        {
-          number: 9,
-        },
-        {
-          number: 11,
-        },
-        {
-          number: 13,
-        },
-      ],
-    },
-  ];
-
-  const bookChapterNonConsecutiveSongOfSolomon = "Song of Solomon97,101,103";
-  const bookChapterNonConsecutiveSongOfSolomonExpected: Book[] = [
-    {
-      name: "Song of Solomon",
-      chapters: [
-        {
-          number: 97,
-        },
-        {
-          number: 101,
-        },
-        {
-          number: 103,
-        },
-      ],
-    },
-  ];
-
-  const bookChapterNonConsecutiveVerseGenesis = "Genesis 1:1, 3, 5";
-  const bookChapterNonConsecutiveVerseGenesisExpected: Book[] = [
-    {
-      name: "Genesis",
-      chapters: [
-        {
-          number: 1,
-          verses: [{ number: 1 }, { number: 3 }, { number: 5 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterNonConsecutiveVerse1John = "1  John  11 : 11 ,  13 ,   15";
-  const bookChapterNonConsecutiveVerse1JohnExpected: Book[] = [
-    {
-      name: "1 John",
-      chapters: [
-        {
-          number: 11,
-          verses: [{ number: 11 }, { number: 13 }, { number: 15 }],
-        },
-      ],
-    },
-  ];
-
-  const bookChapterNonConsecutiveVerseSongOfSolomon =
-    "Song of Solomon97:101,103,105";
-  const bookChapterNonConsecutiveVerseSongOfSolomonExpected: Book[] = [
-    {
-      name: "Song of Solomon",
-      chapters: [
-        {
-          number: 97,
-          verses: [{ number: 101 }, { number: 103 }, { number: 105 }],
-        },
-      ],
-    },
-  ];
-
   it("should return an equivalent search object for each search passed to it", () => {
     // arrange
-    const searchExpectations = [
-      {
-        search: bookGenesis,
-        expectation: bookGenesisExpected,
+    // Just add what you want to search for as a new key, and the expected return value as the value
+    const searches: Record<string, Book> = {
+      Genesis: {
+        name: "Genesis",
+        chapters: [],
       },
-      {
-        search: book1John,
-        expectation: book1JohnExpected,
+      "1John": {
+        name: "1 John",
+        chapters: [],
       },
-      {
-        search: bookSongOfSolomon,
-        expectation: bookSongOfSolomonExpected,
+      "Song Of Solomon": {
+        name: "Song Of Solomon",
+        chapters: [],
       },
-      {
-        search: book11John,
-        expectation: book11JohnExpected,
+      "Genesis 1": {
+        name: "Genesis",
+        chapters: [{ number: 1, verses: [] }],
       },
-      {
-        search: bookChapterGenesis,
-        expectation: bookChapterGenesisExpected,
+      "1John11": {
+        name: "1 John",
+        chapters: [
+          {
+            number: 11,
+            verses: [],
+          },
+        ],
       },
-      {
-        search: bookChapter1John,
-        expectation: bookChapter1JohnExpected,
+      "Song of Solomon 111": {
+        name: "Song of Solomon",
+        chapters: [
+          {
+            number: 111,
+            verses: [],
+          },
+        ],
       },
-      {
-        search: bookChapterSongOfSolomon,
-        expectation: bookChapterSongOfSolomonExpected,
+      "Genesis 1:1": {
+        name: "Genesis",
+        chapters: [
+          {
+            number: 1,
+            verses: [{ number: 1 }],
+          },
+        ],
       },
-      {
-        search: bookChapterVerseGenesis,
-        expectation: bookChapterVerseGenesisExpected,
+      "1 John 11:11": {
+        name: "1 John",
+        chapters: [
+          {
+            number: 11,
+            verses: [{ number: 11 }],
+          },
+        ],
       },
-      {
-        search: bookChapterVerse1John,
-        expectation: bookChapterVerse1JohnExpected,
+      "Song Of Solomon 111:111": {
+        name: "Song Of Solomon",
+        chapters: [
+          {
+            number: 111,
+            verses: [{ number: 111 }],
+          },
+        ],
       },
-      {
-        search: bookChapterVerseSongOfSolomon,
-        expectation: bookChapterVerseSongOfSolomonExpected,
+      "Genesis 1-3": {
+        name: "Genesis",
+        chapters: [
+          {
+            number: 1,
+            verses: [],
+          },
+          {
+            number: 2,
+            verses: [],
+          },
+          {
+            number: 3,
+            verses: [],
+          },
+        ],
       },
-      {
-        search: bookChapterRangeGenesis,
-        expectation: bookChapterRangeGenesisExpected,
+      "1 John 11 - 13": {
+        name: "1 John",
+        chapters: [
+          {
+            number: 11,
+            verses: [],
+          },
+          {
+            number: 12,
+            verses: [],
+          },
+          {
+            number: 13,
+            verses: [],
+          },
+        ],
       },
-      {
-        search: bookChapterRange1John,
-        expectation: bookChapterRange1JohnExpected,
+      "Song of Solomon111 - 113": {
+        name: "Song of Solomon",
+        chapters: [
+          {
+            number: 111,
+            verses: [],
+          },
+          {
+            number: 112,
+            verses: [],
+          },
+          {
+            number: 113,
+            verses: [],
+          },
+        ],
       },
-      {
-        search: bookChapterRangeSongOfSolomon,
-        expectation: bookChapterRangeSongOfSolomonExpected,
+      "Genesis 1:1-3": {
+        name: "Genesis",
+        chapters: [
+          {
+            number: 1,
+            verses: [{ number: 1 }, { number: 2 }, { number: 3 }],
+          },
+        ],
       },
-      {
-        search: bookChapterVerseRangeGenesis,
-        expectation: bookChapterVerseRangeGenesisExpected,
+      "1  John  11 :  11  -  13": {
+        name: "1 John",
+        chapters: [
+          {
+            number: 11,
+            verses: [{ number: 11 }, { number: 12 }, { number: 13 }],
+          },
+        ],
       },
-      {
-        search: bookChapterVerseRange1John,
-        expectation: bookChapterVerseRange1JohnExpected,
+      "Song of Solomon111:111-113": {
+        name: "Song of Solomon",
+        chapters: [
+          {
+            number: 111,
+            verses: [{ number: 111 }, { number: 112 }, { number: 113 }],
+          },
+        ],
       },
-      {
-        search: bookChapterVerseRangeSongOfSolomon,
-        expectation: bookChapterVerseRangeSongOfSolomonExpected,
+      "Genesis 1, 3, 5": {
+        name: "Genesis",
+        chapters: [
+          {
+            number: 1,
+          },
+          {
+            number: 3,
+          },
+          {
+            number: 5,
+          },
+        ],
       },
-      {
-        search: bookChapterNonConsecutiveGenesis,
-        expectation: bookChapterNonConsecutiveGenesisExpected,
+      "1  John  9,  11,  13": {
+        name: "1 John",
+        chapters: [
+          {
+            number: 9,
+          },
+          {
+            number: 11,
+          },
+          {
+            number: 13,
+          },
+        ],
       },
-      {
-        search: bookChapterNonConsecutive1John,
-        expectation: bookChapterNonConsecutive1JohnExpectation,
+      "Song of Solomon97,101,103": {
+        name: "Song of Solomon",
+        chapters: [
+          {
+            number: 97,
+          },
+          {
+            number: 101,
+          },
+          {
+            number: 103,
+          },
+        ],
       },
-      {
-        search: bookChapterNonConsecutiveSongOfSolomon,
-        expectation: bookChapterNonConsecutiveSongOfSolomonExpected,
+      "Genesis 1:1, 3, 5": {
+        name: "Genesis",
+        chapters: [
+          {
+            number: 1,
+            verses: [{ number: 1 }, { number: 3 }, { number: 5 }],
+          },
+        ],
       },
-      {
-        search: bookChapterNonConsecutiveVerseGenesis,
-        expectation: bookChapterNonConsecutiveVerseGenesisExpected,
+      "1  John  11 : 11 ,  13 ,   15": {
+        name: "1 John",
+        chapters: [
+          {
+            number: 11,
+            verses: [{ number: 11 }, { number: 13 }, { number: 15 }],
+          },
+        ],
       },
-      {
-        search: bookChapterNonConsecutiveVerse1John,
-        expectation: bookChapterNonConsecutiveVerse1JohnExpected,
+      "Song of Solomon97:101,103,105": {
+        name: "Song of Solomon",
+        chapters: [
+          {
+            number: 97,
+            verses: [{ number: 101 }, { number: 103 }, { number: 105 }],
+          },
+        ],
       },
-      {
-        search: bookChapterNonConsecutiveVerseSongOfSolomon,
-        expectation: bookChapterNonConsecutiveVerseSongOfSolomonExpected,
-      },
-    ];
+    };
 
-    const searches = searchExpectations.map((s) => s.search).join(";");
-    const expectations = searchExpectations.flatMap((s) => s.expectation);
+    const searchString = Object.entries(searches)
+      .map((entry) => entry[0])
+      .join(";");
+
+    const expected = Object.entries(searches).map((entry) => entry[1]);
 
     // act
-    const result = getSearches(searches);
-
     // assert
-    expect(result).toEqual(expectations);
+    expect(getSearches(searchString)).toEqual(expected);
   });
 });
--- src/search/search.ts
+++ src/search/search.ts
@@ -5,23 +5,13 @@ import { getSearches } from "./string-to-searches";
 
 const bibleSearchTransaction = async (
   selects: Array<Prisma.BookFindManyArgs>
-) => {
-  console.log(`selects ${selects}`);
-  return prisma.$transaction(
-    selects.map((s) => {
-      return prisma.book.findMany(s);
-    })
-  );
-};
+) => prisma.$transaction(selects.map((s) => prisma.book.findMany(s)));
 
 const search = async (search: string) => {
-  console.log(`search: ${search}`);
   const searches = getSearches(search);
-  console.log(`searches: ${searches}`);
   const prismaQueries = toPrisma(searches).map((pq) => pq.search);
-  console.log(`prismaQueries: ${prismaQueries}`);
   const result = await bibleSearchTransaction(prismaQueries);
-  console.log(`result: ${result}`);
+
   return result;
 };
 
